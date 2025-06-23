6249e4629c79383bc1c7fbe5a4359393372f2e33
cracvision <cracvision@gmail.com>
2025-06-23 15:12:31 -0400

    Switch tests to Node built-in runner

---
+++ src/public/test/renderMensaje.test.js
@@ -0,0 +1,33 @@
+import { test } from 'node:test';
+import assert from 'assert/strict';
+import { readFile } from 'fs/promises';
+import vm from 'vm';
+
+const html = await readFile(new URL('../boriChatEN.html', import.meta.url), 'utf8');
+const scriptMatch = html.match(/<script>([\s\S]*?)<\/script>/i);
+const code = scriptMatch[1] + '\nmodule.exports = { sanitizeHTML };';
+const sandbox = { module: { exports: {} } };
+vm.createContext(sandbox);
+vm.runInContext(code, sandbox, { filename: 'boriChatEN.html' });
+const { sanitizeHTML } = sandbox.module.exports;
+
+test('sanitizeHTML keeps safe links intact', () => {
+  assert.strictEqual(
+    sanitizeHTML('<p><a href="https://example.com">link</a></p>'),
+    '<p><a href="https://example.com">link</a></p>'
+  );
+});
+
+test('sanitizeHTML preserves nested anchors', () => {
+  assert.strictEqual(
+    sanitizeHTML('<div><strong><a href="http://safe.com">safe</a></strong></div>'),
+    '<div><strong><a href="http://safe.com">safe</a></strong></div>'
+  );
+});
+
+test('sanitizeHTML strips malicious hrefs', () => {
+  assert.strictEqual(
+    sanitizeHTML('<p><a href="javascript:alert(1)">bad</a></p>'),
+    '<p><a>bad</a></p>'
+  );
+});
