0fbacf3841b2f4e3a89e96d78f5267cd2a2c6f3e
cracvision <cracvision@gmail.com>
2025-06-29 09:17:18 -0400

    feat: sync UI for Bori-Shop button

--- src/pages/Lightbox_PaymentIframe.k9mt2.js
+++
@@ -1,8 +0,0 @@
-import { lightbox } from 'wix-window';
-
-$w.onReady(function () {
-  const ctx = lightbox.getContext();
-  if (ctx && ctx.url && $w('#htmlPaymentFrame')) {
-    $w('#htmlPaymentFrame').src = ctx.url;
-  }
-});
